b824124f8f761c3b72b55ab02ec4f31f8d411d5d
cijiugechu <cijiugechu@gmail.com>
2023-02-14 21:29:41 +0800

    refactor: use `satori-html` to output satori

--- packages/core/ssg.ts
+++ packages/core/ssg.ts
@@ -1,133 +1,101 @@
 import type { SatoriOptions } from 'satori'
 import type { FrontMatter } from './types.js'
 
-const defaultOgImageELement = ({ title, description, author }: FrontMatter) => {
-  return {
-    type: 'div',
-    props: {
-      style: {
-        background: '#fefbfb',
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontFamily: 'IBM Plex Mono',
-      },
-      children: [
-        {
-          type: 'div',
-          props: {
-            style: {
-              position: 'absolute',
-              top: '-1px',
-              right: '-1px',
-              border: '4px solid #000',
-              background: '#ecebeb',
-              opacity: '0.9',
-              borderRadius: '4px',
-              display: 'flex',
-              justifyContent: 'center',
-              margin: '2.5rem',
-              width: '88%',
-              height: '80%',
-            },
-          },
-        },
-        {
-          type: 'div',
-          props: {
-            style: {
-              border: '4px solid #000',
-              background: '#fefbfb',
-              borderRadius: '4px',
-              display: 'flex',
-              justifyContent: 'center',
-              margin: '2rem',
-              width: '88%',
-              height: '80%',
-            },
-            children: [
-              {
-                type: 'div',
-                props: {
-                  style: {
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'space-between',
-                    margin: '20px',
-                    width: '90%',
-                    height: '90%',
-                  },
-                  children: [
-                    {
-                      type: 'p',
-                      props: {
-                        style: {
-                          fontSize: 72,
-                          fontWeight: 'bold',
-                          maxHeight: '84%',
-                          overflow: 'hidden',
-                        },
-                        children: [title],
-                      },
-                    },
-                    {
-                      type: 'div',
-                      props: {
-                        style: {
-                          display: 'flex',
-                          justifyContent: 'space-between',
-                          width: '100%',
-                          marginBottom: '8px',
-                          fontSize: 28,
-                        },
-                        children: [
-                          {
-                            type: 'span',
-                            children: [
-                              'by',
-                              '  ',
-                              {
-                                type: 'span',
-                                props: {
-                                  style: {
-                                    color: 'transparent',
-                                  },
-                                  children: [`"`],
-                                },
-                              },
-                              {
-                                type: 'span',
-                                props: {
-                                  style: {
-                                    overflow: 'hidden',
-                                    fontWeight: 'bold',
-                                  },
-                                  children: [author],
-                                },
-                              },
-                            ],
-                          },
-                          {
-                            type: 'span',
-                            props: {
-                              style: { overflow: 'hidden', fontWeight: 'bold' },
-                              children: [title],
-                            },
-                          },
-                        ],
-                      },
-                    },
-                  ],
-                },
-              },
-            ],
-          },
-        },
-      ],
-    },
-  }
+import { html } from 'satori-html'
+
+type VNODE = ReturnType<typeof html>
+
+const defaultOgImageELement = ({ title, description, author }: FrontMatter): VNODE => {
+  return html`
+  <div
+      style="
+        background: #fefbfb;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      "
+    >
+      <div
+        style="
+          position: absolute;
+          top: -1px;
+          right: -1px;
+          border: 4px solid #000;
+          background: #ecebeb;
+          opacity: 0.9;
+          border-radius: 4px;
+          display: flex;
+          justify-content: center;
+          margin: 2.5rem;
+          width: 88%;
+          height: 80%;
+        "
+      />
+      <div
+        style="
+          border: 4px solid #000;
+          background: #fefbfb;
+          border-radius: 4px;
+          display: flex;
+          justify-content: center;
+          margin: 2rem;
+          width: 88%;
+          height: 80%;
+        "
+      >
+        <div
+          style="
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            margin: 20px;
+            width: 90%;
+            height: 90%;
+          "
+        >
+          <p
+            style="
+              font-size: 72px;
+              font-weight: bold;
+              max-height: 84%;
+              overflow: hidden;
+            "
+          >
+            ${title}
+          </p>
+          <div
+            style="
+              display: flex;
+              justify-content: space-between;
+              width: 100%;
+              margin-bottom: 8px;
+              font-size: 28px;
+            "
+          >
+            <span>
+              by  
+              <span
+                style="
+                  color: transparent;
+                "
+              >
+                "
+              </span>
+              <span style="overflow: hidden; fontWeight: bold; ">
+                ${author}
+              </span>
+            </span>
+
+            <span style=" overflow: hidden; font-weight: bold; ">
+              ${title}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  `
 }
 
 const defaultGenerateOptions = async () => {
